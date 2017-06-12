3308811b4699c1b5406ae1c81d3ecdad2b9a3ee6
Tomasz Mikus <mikus.tomasz@gmail.com>
2017-06-12 20:39:33 +0100

    Improving error handling for webstorm.bat helper.

--- webstorm.bat
+++ webstorm.bat
@@ -1,6 +1,16 @@
 @echo off
 
-SET WebstormVersionsDir=%UserProfile%\AppData\Local\JetBrains\Toolbox\apps\WebStorm\ch-0
+SET JBToolboxDir=%UserProfile%\AppData\Local\JetBrains\Toolbox
+IF NOT EXIST %JBToolboxDir% (
+    ECHO JetBrains Toolbox could not be found.
+    EXIT 1
+)
+
+SET WebstormVersionsDir=%JBToolboxDir%\apps\WebStorm\ch-0
+IF NOT EXIST %WebstormVersionsDir% (
+    ECHO Could not find WebStorm installation. This tool only works with JetBrains Toolbox installations of WebStorm.
+    Exit 1
+)
 
 FOR /F "eol=" %%i IN ('DIR %WebstormVersionsDir% /O:D /b /A:D') DO (
     SET WebstormVersion=%%i
@@ -9,6 +19,10 @@ FOR /F "eol=" %%i IN ('DIR %WebstormVersionsDir% /O:D /b /A:D') DO (
 
 :versionFound
 SET LatestWebstorm=%WebstormVersionsDir%\%WebstormVersion%\bin\webstorm64.exe
+IF NOT EXIST %LatestWebstorm% (
+    ECHO Could not find WebStorm installation. This tool only works with JetBrains Toolbox installations of WebStorm.
+    Exit 1
+)
 
 START %LatestWebstorm% %*
 EXIT
